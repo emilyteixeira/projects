99a14c8c5fc64af04dd2fef04a80556c324cb165
Emily Teixeira <milysillva@gmail.com>
2023-03-15 18:34:46 -0300

    Alterando algumas coisas

--- afazeres/afazeres/src/App.vue
+++ afazeres/afazeres/src/App.vue
@@ -1,15 +1,15 @@
-<script setup lang="ts">
-import {RouterView } from 'vue-router'
-
+<script setup>
+import { RouterView } from "vue-router";
+import TodoHeader from "./components/TodoHeader.vue";
 </script>
 
 <template>
+  <TodoHeader />
   <RouterView />
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@600&display=swap');
-
+@import url("https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap");
 * {
   font-family: "Rubik", sans-serif;
   margin: 0;
